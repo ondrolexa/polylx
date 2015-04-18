0e0c18a1e8d2a3dfc74fdec0223576f0a630f38c
Ondrej Lexa <lexa.ondrej@gmail.com>
2015-04-19 01:45:32 +0200

    mocking

--- docs/conf.py
+++ docs/conf.py
@@ -318,6 +318,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['shapely', 'shapely.geometry']
+MOCK_MODULES = ['shapely', 'shapely.geometry', 'networkx']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
