c0ea9de3f79e337021fc0ed6640e53f5d8512a7c
ondrolexa <lexa.ondrej@gmail.com>
2017-01-12 21:34:34 +0100

    release fix

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=requirements,
     entry_points={
     'console_scripts': [
-        'iapsg=apsg.shell:main'
+        'ipolylx=polylx.shell:main'
         ]
     },
     license="BSD",
