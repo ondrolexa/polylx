21c3da7e9cd99b514f9239367831fe5584d78e5d
ondrolexa <lexa.ondrej@gmail.com>
2017-03-05 13:44:52 +0100

    Bump version: 0.4.7 → 0.4.8

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='polylx',
-    version='0.4.7',
+    version='0.4.8',
     description="A Python package to visualize and analyze microstructures.",
     long_description=readme + '\n\n' + history,
     author="Ondrej Lexa",
