2691e07c8df8b0eb0d579fe4f335b6321d0d66ac
Ondrej Lexa <lexa.ondrej@gmail.com>
2024-02-16 11:22:09 +0100

    test fix

--- tests/test_polylx.py
+++ tests/test_polylx.py
@@ -16,7 +16,6 @@ sample = Sample.example()
 
 
 class TestPolylx(unittest.TestCase):
-
     def setUp(self):
         pass
 
@@ -27,11 +26,21 @@ class TestPolylx(unittest.TestCase):
         self.assertTrue((len(sample.b) == 1901) & (len(sample.b.names) == 6))
 
     def test_boundaries_lengths(self):
-        eq = np.array([np.allclose(sample.b[sample.bids(i)].length.sum(), sample.g[i].length) for i in range(len(sample.g))])
+        eq = np.array(
+            [
+                np.allclose(sample.b[sample.bids(i)].length.sum(), sample.g[i].length)
+                for i in range(len(sample.g))
+            ]
+        )
         self.assertTrue(len(sample.g[~eq]) == 124)
 
     def test_grain_read_shapefile(self):
-        ex = (0.77740719118042412, 0.048953149577128548, 3.7836452599329293, 2.0781454198574365)
+        ex = (
+            0.77740719118042412,
+            0.048953149577128548,
+            3.7836452599329293,
+            2.0781454198574365,
+        )
         self.assertTrue(np.allclose(sample.g.extent, ex))
 
     def test_grain_axialratio(self):
@@ -44,32 +53,50 @@ class TestPolylx(unittest.TestCase):
         self.assertTrue(np.allclose(sample.g.ead.mean(), 0.072811839410185208))
 
     def test_circular_mean(self):
-        self.assertTrue(np.allclose(circular.mean(sample.g['pl'].lao), 94.19784708988459))
+        self.assertTrue(
+            np.allclose(circular.mean(sample.g["pl"].lao), 94.19784708988459)
+        )
 
     def test_paror(self):
-        self.assertTrue(np.allclose(sample.g.paror(normalized=False).mean(axis=1).mean(), 0.21099580521756531))
+        self.assertTrue(
+            np.allclose(
+                sample.g.paror(normalized=False).mean(axis=1).mean(),
+                0.21099580521756531,
+            )
+        )
 
     def test_surfor(self):
-        self.assertTrue(np.allclose(sample.g.surfor(normalized=False).mean(axis=1).mean(), 0.091372541656203621))
+        self.assertTrue(
+            np.allclose(
+                sample.g.surfor(normalized=False).mean(axis=1).mean(),
+                0.091372541656203621,
+            )
+        )
 
-    def test_smooth_chaikin(self):
+    def test_generalize_chaikin(self):
         try:
-            sample.g.smooth()
+            sample.g.generalize(method="chaikin")
         except Exception:
-            self.fail('Chaikin corner-cutting smoothing failed.')
+            self.fail("Chaikin corner-cutting generalization failed.")
 
-    def test_smooth_vw(self):
+    def test_generalize_vw(self):
         try:
-            sample.g.simplify()
+            sample.g.generalize(method="vw")
         except Exception:
-            self.fail('Visvalingam-Whyatt simplification failed.')
+            self.fail("Visvalingam-Whyatt generalization failed.")
+
+    def test_generalize_taubin(self):
+        try:
+            sample.g.generalize(method="taubin")
+        except Exception:
+            self.fail("Taubin generalization failed.")
 
     def test_regularization(self):
         try:
             sample.g.regularize()
         except Exception:
-            self.fail('Regularization failed.')
+            self.fail("Regularization failed.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
